38184e54081ee0596a360aeab33f999698f1446d
Houra <houra.mehdipour@gmail.com>
2019-10-26 02:46:33 +0330

    add drawerlayout

--- app/src/main/java/com/example/sematecholymoonmenu/MainActivity.java
+++ app/src/main/java/com/example/sematecholymoonmenu/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.sematecholymoonmenu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnDrawerMenu = findViewById(R.id.btnDrawerMenu);
+        btnDrawerMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+//To Open:
+                drawerLayout.openDrawer(GravityCompat.START);
+
+            }
+        });
     }
 }
